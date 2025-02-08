7044d736991258a88368d4288ce01128cd748c59
Pramod Modi <pramodkmodi@gmail.com>
2025-02-08 15:10:18 +0530

    Added flask

--- pythonbasic/main.py
+++ pythonbasic/main.py
@@ -1,1 +1,10 @@
-print("Hello LLMApp")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello LLM App at Google App Engine!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
